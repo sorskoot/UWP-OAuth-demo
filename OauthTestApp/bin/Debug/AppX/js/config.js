73d738ffba5613e7e8ef342e72714993ed6feb11
Sorskoot <mental_c@hotmail.com>
2015-11-13 00:14:29 +0100

    Small changes to make the code work better in the video.

---
+++ OauthTestApp/bin/Debug/AppX/js/config.js
@@ -0,0 +1,12 @@
+﻿window.youtubeConfig = {
+    base_url: "https://www.googleapis.com/youtube/v3/",
+    token_uri: "https://accounts.google.com/o/oauth2/token",
+    client_id: "X",
+    client_secret: "X",
+    redirect_uris: ["https://localhost/oauth2callback"],
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+}
+
+// Google developer console - https://console.developers.google.com/
+// How to obtain credentials - https://developers.google.com/youtube/registering_an_application
+// More on the oauth flow - https://developers.google.com/youtube/v3/guides/auth/installed-apps
---
+++ OauthTestApp/js/configUntracked.js
@@ -0,0 +1,8 @@
+﻿window.youtubeConfig = {
+    base_url: "https://www.googleapis.com/youtube/v3/",
+    token_uri: "https://accounts.google.com/o/oauth2/token",
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    redirect_uris: ["https://localhost/oauth2callback"],
+    client_id: "136057650672-9gfvjivuau0hu6d60qkm4koulpjfk16p.apps.googleusercontent.com",
+    client_secret: "ZuQk4dqca6rNxUErf3bayGFc",
+}
